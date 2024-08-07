c4039963934eb8a8a1b6965ce50e60cfd239952f
mshymch <mateuszymczak@o2.pl>
2024-08-07 16:07:48 +0200

    Add total amount in expense summary

--- expenses/reports.py
+++ expenses/reports.py
@@ -5,7 +5,7 @@ from django.db.models.functions import Coalesce
 
 
 def summary_per_category(queryset):
-    return OrderedDict(sorted(
+    category_summary = OrderedDict(sorted(
         queryset
         .annotate(category_name=Coalesce('category__name', Value('-')))
         .order_by()
@@ -13,4 +13,6 @@ def summary_per_category(queryset):
         .annotate(s=Sum('amount'))
         .values_list('category_name', 's')
     ))
+    category_summary['Total'] = queryset.aggregate(total=Sum('amount'))['total']
 
+    return category_summary
